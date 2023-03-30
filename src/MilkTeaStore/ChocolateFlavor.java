654a1b38d7b78b244402bc5a347d13926cbf163a
duyykhanhh <18130107@st.hcmuaf.edu.vn>
2023-03-30 17:24:05 +0700

    first commit

---
+++ src/MilkTeaStore/ChocolateFlavor.java
@@ -0,0 +1,19 @@
+package MilkTeaStore;
+
+public class ChocolateFlavor implements IFlavorStrategy{
+private int price;
+	@Override
+	public String getFlavorDiscription() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public double getPrice() {
+		// TODO Auto-generated method stub
+		
+
+		return 0;
+	}
+
+}
